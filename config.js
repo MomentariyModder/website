8112954ac66cb58cc71b3772e778c2853c1c88bc
MomentariyModder <82979292+MomentariyModder@users.noreply.github.com>
2023-01-08 07:45:21 +0300

    Website 2.0: R1

---
+++ config.js
@@ -0,0 +1,17 @@
+/*!
+ * Quara Theme by OllieJW (https://olliejw.me)
+ * License - https://olliejw.me/tos
+ */
+
+var server_name = "MomentariyModder";
+var server_ip = "0.0.0.0";
+var server_port = 25565;
+var ip_copied = "IP copied!";
+
+var firefly_count = 20;
+
+
+vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
+vote("Minecraft Servers Org", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraftservers.org/");
+vote("Minecraft MP", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraft-mp.com/");
+vote("Top Minecraft Servers", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://topminecraftservers.org/");
